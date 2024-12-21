e70f0461f868df884c18e33addefc12e0c752938
MuhammadAhsanShahbaz <rmahsan182@gmail.com>
2024-12-21 12:26:19 +0500

    Modify the code

--- Scrapy-Assignment/CircuitBreakers/CircuitBreakers/CircuitBreakers/spiders/CircuitBreakers.py
+++ Scrapy-Assignment/CircuitBreakers/CircuitBreakers/CircuitBreakers/spiders/CircuitBreakers.py
@@ -1,48 +1,48 @@
-import scrapy
 from scrapy import Request, Spider
-from scrapy.http import FormRequest
 from ..items import CircuitbreakersItem
-from scrapy.utils.response import open_in_browser
+
 
 class CircuitBreakers(Spider):
     name = 'CircuitBreakers'
-    start_urls = [
-        'https://ktcables.com.au/'
-    ]
+    start_urls = ['https://ktcables.com.au/']
 
-    def parse(self, response):
+    def start_requests(self):
+        yield Request(url=self.start_urls[0],
+                      callback=self.parse)
 
-        product = response.css(".navbar__category-col a::attr(href)").extract()
+    def parse(self, response, **kwargs):
+        products = response.css(".navbar__category-col a::attr(href)").extract()
 
-        for p in product:
-
-            next_pages = 'https://ktcables.com.au' + str(p)
-            yield Request(next_pages, callback=self.parse_product)
+        for product in products:
+            next_pages = 'https://ktcables.com.au' + str(product)
+            yield Request(url=next_pages,
+                          callback=self.parse_product)
 
     def parse_product(self, response):
-        #self.logger.info("Parse function called on %s", response.url)
+        all_div = response.css(".woocommerce-loop-product__link::attr(href)").getall()
 
-        all_div = response.css(".woocommerce-loop-product__link::attr(href)").extract()
+        next_pages = response.css('ul .page-numbers::attr(href)').get('')
 
-        next_pages = response.css('ul .page-numbers::attr(href)').extract()
-
-        for href in all_div:
-
+        for product_url in all_div:
             items = CircuitbreakersItem()
-            items['href'] = href
-            yield Request(href, callback=self.detail_scrap, meta={'items': items})
 
+            items['href'] = product_url
+
+            yield Request(url=product_url,
+                          callback=self.detail_scrap,
+                          meta={'items': items})
+
+        # Pagination for all pages
         for next_page in next_pages:
             yield Request(next_page, callback=self.parse_product)
 
 
     def detail_scrap(self, response):
-
-        #self.logger.info("Parse function called on %s", response.url)
-
         items = response.meta['items']
+
         items['categories'] = response.css("span a::text").extract_first()
         items['image'] = response.css("a .wp-post-image::attr(src)").extract_first()
         items['sku'] = response.css("span span::text").get()
         items['description'] = response.css(".panel-body p::text").get()
+
         yield items
